dbdb95c39386efd8bcce86384d19ab5be0e75e30
evitalis <evitalis@users.noreply.github.com>
2015-03-17 20:20:10 -0500

    PEP8 compatibility

--- steamclean.py
+++ steamclean.py
@@ -1,7 +1,7 @@
 ﻿#!/usr/bin/env python3
 
 # Filename:     steamclean.py
-# Version:      0.2.2
+# Version:      0.2.3
 # Release Date: 2014.02.02
 # Description:  Script to find and remove extraneous files from
 #               Steam game installations.
@@ -44,9 +44,14 @@ def win_reg_check():
     try:
         # Open key based on architecture returned from platform.
         if '64' in pa()[0]:
-            regkey = winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\Wow6432Node\Valve\Steam', 0, (winreg.KEY_WOW64_64KEY + winreg.KEY_READ))
+            regkey = winreg.OpenKeyEx(
+                winreg.HKEY_LOCAL_MACHINE,
+                r'SOFTWARE\Wow6432Node\Valve\Steam', 0,
+                (winreg.KEY_WOW64_64KEY + winreg.KEY_READ))
         elif '32' in pa()[0]:
-            regkey = winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\Valve\Steam', 0, (winreg.KEY_WOW64_64KEY + winreg.KEY_READ))
+            regkey = winreg.OpenKeyEx(
+                winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\Valve\Steam', 0,
+                (winreg.KEY_WOW64_64KEY + winreg.KEY_READ))
         # else block should never be reached under normal conditions.
         else:
             raise ValueError('Invalid or missing architecture.')
@@ -77,7 +82,8 @@ def analyze_vdf(steamdir, dirclean=False, library=None):
 
     # Ensure that the provided path for steamdir is valid.
     while os.path.isdir(steamdir) and 'Steam' not in steamdir:
-        steamdir = os.path.abspath(input('Invalid Steam directory, please re-enter the directory: '))
+        steamdir = os.path.abspath(
+            input('Invalid Steam directory, please re-enter the directory: '))
 
     # Validate Steam installation path.
     if os.path.isdir(steamdir) and 'Steam' in steamdir:
@@ -103,7 +109,8 @@ def analyze_vdf(steamdir, dirclean=False, library=None):
 
         # Append game directories found in specified library if present.
         print('Checking library directories. Please wait...')
-        if library is not None and sappscommon in library and os.path.isdir(library):
+        if library is not None and sappscommon in library and
+        os.path.isdir(library):
             for d in os.listdir(library):
                 if os.path.isdir(os.path.join(library, d)):
                     if d not in gamedir:
@@ -117,12 +124,14 @@ def analyze_vdf(steamdir, dirclean=False, library=None):
                 pathlist = os.path.abspath(game + '\\' + item)
                 if os.path.isdir(pathlist) and os.path.exists(pathlist):
                     # Verify the files being added are valid and exist.
-                    if ('directx' in item or 'redist' in item or 'Redist' in item) and 'Miles' not in item:
+                    if ('directx' in item or 'redist' in item or
+                            'Redist' in item) and 'Miles' not in item:
                         # Check subdirectories or applicable files.
                         for (path, dirs, files) in os.walk(game + '\\' + item):
                             for file in files:
                                 filepath = os.path.abspath(path + '\\' + file)
-                                if os.path.isfile(filepath) and os.path.exists(filepath):
+                                if os.path.isfile(filepath) and
+                                os.path.exists(filepath):
                                     redistfiles.append(filepath)
 
         # Add filename and size to cleanable list.
@@ -150,16 +159,20 @@ def analyze_vdf(steamdir, dirclean=False, library=None):
                 if 'INSTALLDIR' in line:
                     # Replace %INSTALLDIR% with path and make it valid.
                     splitline = line.split('%')
-                    newline = splitline[1].replace('INSTALLDIR', game) + splitline[2][0: splitline[2].find('.') + 4]
+                    newline = splitline[1].replace('INSTALLDIR', game) +
+                    splitline[2][0: splitline[2].find('.') + 4]
 
-                    # Sanitize path and append only existing files to clean list.
+                    # Clean path, appending only existing files to clean list.
                     filepath = os.path.abspath(newline)
                     if os.path.isfile(filepath) and os.path.exists(filepath):
                         filepath = filepath.lower()
-                        # Check filename to determine if it is a redistributable before adding to cleanable to ensure a required file is not removed.
+                        # Check filename to determine if it is a
+                        # redistributable before adding to cleanable to
+                        # ensure a required file is not removed.
                         for rc in ['setup', 'redist']:
                             if rc in filepath:
-                                cleanable[filepath] = ((os.path.getsize(filepath) / 1024) / 1024)
+                                cleanable[filepath] = (
+                                    (os.path.getsize(filepath) / 1024) / 1024)
 
     # Return the list of cleanable files and their approximate size.
     return cleanable
@@ -167,7 +180,8 @@ def analyze_vdf(steamdir, dirclean=False, library=None):
 
 def clean_data(filelist, printlist=False):
     """ Function to remove found data from installed game directories.
-        Will prompt user for a list of files to exclude with the proper options otherwise all will be deleted."""
+        Will prompt user for a list of files to exclude with the proper
+        options otherwise all will be deleted."""
 
     summary_report(filelist)
     confirm = ''
@@ -178,27 +192,35 @@ def clean_data(filelist, printlist=False):
         for index, file in enumerate(filelist):
             print(index, file, '\n')
 
-        # Request index of all files that should be excluded from deletion and ensure it is valid for the number of files present.
+        # Request index of all files that should be excluded from
+        # deletion and ensure it is valid for the number of files present.
         if len(filelist) > 0:
             while True:
-                userinput = input('Enter file number to exclude from deletion (no input to continue): ')
-                if userinput != '' and int(userinput) <= len(filelist) and int(userinput) >= 0:
+                userinput = input(
+                    'Enter file number to exclude from deletion \
+                    (no input to continue): ')
+                if userinput != '' and int(userinput) <= len(filelist) and
+                int(userinput) >= 0:
                     if int(userinput) not in excludes:
                         excludes.append(int(userinput))
                     continue
                 else:
                     break
 
-    # Print a warning that files will be permanantly deleted and inform user they can exclude files with the -p option.
-    print('\nWARNING: All files will be permanantly deleted! If you wish to review the list of files to be removed please re-run this script with the -p option.\n')
+    # Print a warning that files will be permanantly deleted and
+    # inform user they can exclude files with the -p option.
+    print('\nWARNING: All files will be permanantly deleted! If you wish to \
+    review the list of files to be removed please re-run this \
+    script with the -p option.\n')
     while True:
-        confirm = input('Do you wish to remove all found files [y/N]: ').lower()
+        confirm = input('Do you wish to remove extra files [y/N]: ').lower()
         if confirm != 'y' and confirm != 'n':
             continue
         else:
             break
 
-    # Confirm removal of all found files. Print list of files not removed and count of removed items.
+    # Confirm removal of all found files. Print list of files not removed and
+    # count of removed items.
     if confirm == 'y':
         removed = 0
 
@@ -214,7 +236,7 @@ def clean_data(filelist, printlist=False):
 
 
 def summary_report(cleanable, printlist=False):
-    """ Print a report of the number of files that can be cleaned and estimated size. """
+    """ Print a report of removable files and thier estimated size. """
 
     filecount = len(cleanable)
     totalsize = 0
@@ -231,11 +253,19 @@ def summary_report(cleanable, printlist=False):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Find and clean unneeded files from Steam game installation directories.')
-    parser.add_argument('-d', '--dirclean', help='Clean redistributable directories if found.', action='store_false')
-    parser.add_argument('-l', '--library', help='Additional Steam library to examine.')
-    parser.add_argument('-p', '--printlist', help='Print list of removable data in clean and summary actions.', action='store_true')
-    parser.add_argument('-s', '--summary', help='Print summary of total number of files and estimated size only. Will print list of files if -p is also specified.', action='store_true')
+    parser = argparse.ArgumentParser(
+        description='Find and clean unneeded files from game directories.')
+    parser.add_argument('-d', '--dirclean',
+                        help='Clean redistributable directories if found.',
+                        action='store_false')
+    parser.add_argument('-l', '--library',
+                        help='Additional Steam library to examine.')
+    parser.add_argument('-p', '--printlist',
+                        help='Print list of removable data.',
+                        action='store_true')
+    parser.add_argument('-s', '--summary',
+                        help='Summary of number of files and total size.',
+                        action='store_true')
     args = parser.parse_args()
 
     print_header()
@@ -249,8 +279,10 @@ if __name__ == "__main__":
             else:
                 clean_data(cleanable, args.printlist)
         else:
-            print('\nCongratulations! No files were found for removal. This script will now exit.')
+            print('\nCongratulations! No files were found for removal. \
+            This script will now exit.')
     elif os.name == 'posix':
-        print('No Linux support at this time. Please report files you believe can be cleaned for consideration.')
+        print('No Linux support at this time. \
+        Please report files that can be cleaned.')
     else:
         print('Invalid operating system detected.')
