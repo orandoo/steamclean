1f73f88530fe5611b42a9c1604128c4f823d6ee2
evitalis <evitalis@users.noreply.github.com>
2016-01-10 14:49:49 -0600

    Automatically detect library function added
    Modified Steam directory check to allow for any directory to be default
    Added function to automatically check for libraries if possible
    New option to auto detect libraries
    Renamed logger to fix messages printing to the console accidentally
    Better handling of provided libraries from different sources
    Error message for non-Windows OS clarified

--- steamclean.py
+++ steamclean.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 # Filename:         steamclean.py
-# Version:          0.5.4
+# Version:          0.6.0
 # Description:      Script to find and remove extraneous files from
 #                   Steam game installation directories.
 
 from codecs import StreamReader
 from datetime import datetime
-from linecache import clearcache, getline
 from platform import architecture as pa
 from platform import platform as pp
 import argparse
@@ -18,17 +17,18 @@ import re
 if (os.name == 'nt'):
     import winreg
 
+VERSION = '0.6.0'   # Global version number as string
 
-# build logger and its configuration to write script data to specified log
-logger = logging.getLogger('steamclean')
-logger.setLevel(logging.INFO)
+# build sclogger and its configuration to write script data to specified log
+sclogger = logging.getLogger('steamclean')
+sclogger.setLevel(logging.INFO)
 logformatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s',
                                  datefmt='%Y-%m-%d %H:%M:%S')
 # use current date and time for log file name for clarity
 timenow = datetime.now().strftime('%Y%m%d-%H%M')
 fh = logging.FileHandler('steamclean_' + timenow + '.log')
 fh.setFormatter(logformatter)
-logger.addHandler(fh)
+sclogger.addHandler(fh)
 
 
 def print_header(filename=None):
@@ -39,26 +39,23 @@ def print_header(filename=None):
     if not filename:
         filename = os.path.basename(__file__)
 
+    header = filename + ' v' + VERSION
+
     if __name__ == '__main__':
         if os.name == 'nt':
             os.system('cls')
         elif os.name == 'posix':
             os.system('clear')
 
-    # Attempt to print script name and modified date as header if able
-    # by reading the local script data
+    # Attempt to write log data and skip upon exception
     try:
-        version = getline(__file__, 4).split(':')[1].strip()
-
-        print('%s v%s \n' % (filename, version))
-        logger.info('Starting script ' + filename + ' v' + version)
+        sclogger.info('Starting %s', header)
+        sclogger.info('Current operating system: ' + pp() + ' ' + pa()[0])
     except:
-        logger.warning('Unable to read script information from file %s',
-                       filename)
-    finally:
-        logger.info('Starting %s', filename)
-        logger.info('Current operating system: ' + pp() + ' ' + pa()[0])
-        print('Starting %s' % (filename))
+        pass
+
+    print('Starting %s' % (header))
+    print('Current operating system: %s %s\n' % (pp(), pa()[0]))
 
 
 def win_reg_check():
@@ -74,12 +71,12 @@ def win_reg_check():
         regpath = r'SOFTWARE\Wow6432Node\Valve\Steam'
         regopts = (winreg.KEY_WOW64_64KEY + winreg.KEY_READ)
     elif arch == '32bit':
-        logger.info('32 bit operating system detected')
+        sclogger.info('32 bit operating system detected')
 
         regpath = r'SOFTWARE\Valve\Steam'
         regopts = winreg.KEY_READ
     else:
-        logger.error('Unable to determine system architecture.')
+        sclogger.error('Unable to determine system architecture.')
         raise ValueError('ERROR: Unable to determine system architecture.')
 
     try:
@@ -89,9 +86,9 @@ def win_reg_check():
         ipath = winreg.QueryValueEx(regkey, 'InstallPath')[0]
 
     except PermissionError:
-        logger.error('Unable to read registry data at %s due to insufficient \
+        sclogger.error('Unable to read registry data at %s due to insufficient \
                  privileges.', regbase + regpath)
-        logger.error('Run this script as administrator to resolve.')
+        sclogger.error('Run this script as administrator to resolve.')
         print('Permission denied to read registry data at %s.', regpath)
 
         ipath = input('Please enter the Steam installation directory: ')
@@ -99,9 +96,9 @@ def win_reg_check():
     finally:
         # Ensure registry key is closed after reading as applicable.
         if regkey is not None:
-            logger.info('Registry data at %s used to determine installation' +
-                        ' path', regbase + regpath)
-            logger.info('Steam installation path found at %s', ipath)
+            sclogger.info('Registry data at %s used to determine ' +
+                          'installation path', regbase + regpath)
+            sclogger.info('Steam installation path found at %s', ipath)
 
             winreg.CloseKey(regkey)
 
@@ -117,28 +114,67 @@ def fix_game_path(dir):
     return os.path.abspath(dir)
 
 
-def find_redist(steamdir, nodir=False, library=None):
+def get_libraries(steamdir):
+    """ Attempt to automatically read extra Steam library directories by
+        checking the libraryfolders.vdf file. """
+
+    libfiledir = os.path.join(steamdir, 'steamapps')
+    # Build the path to libraryfolders.vdf which stores configured libraries.
+    libfile = os.path.abspath(os.path.join(libfiledir, 'libraryfolders.vdf'))
+    # This regex checks for lines starting with the number and grabs the
+    # path specified in that line by matching anything within quotes.
+    libregex = re.compile('(^\t"[1-8]").*(".*")')
+
+    try:
+        libdirs = []
+
+        sclogger.info('Attempting to read libraries from %s', libfile)
+        with open(libfile) as file:
+            for line in file:
+                dir = libregex.search(line)
+                if dir:
+                    # Normalize directory path which is the second match group
+                    ndir = os.path.normpath(dir.group(2))
+                    sclogger.info('Library found at %s', ndir)
+                    libdirs.append(ndir.strip('"'))
+
+        # Return list of any directories found, directories are checked
+        # outside of this function for validity and are ignored if invalid.
+        return libdirs
+    except FileNotFoundError:
+        sclogger.error('Unable to find file %s', libfile, steamdir)
+        print('Unable to find file %s' % (libfile, steamdir))
+    except PermissionError:
+        sclogger.error('Permission denied to %s', libfile)
+        print('Permission denied to %s' % (libfile))
+
+
+def find_redist(steamdir, autolib=False, nodir=False, library=None):
     """ Find all redistributable files and read .vdf files to determine
         all files which can be safely removed. Always check .vdf file
         for removable data but include standard redistributable paths. """
 
-    gamedirs = {}        # list of all valid game directories found
-    cleanable = {}       # list of all files to be removed
+    gamedirs = {}   # list of all valid game directories found
+    cleanable = {}  # list of all files to be removed
+    liblist = []    # list to hold any provided libraries
 
     # validate steamdirectory or prompt for input when invalid or missing
-    while os.path.isdir(steamdir) and 'Steam' not in steamdir:
-        logger.warning('Invalid or missing directory at %s', steamdir)
+    while not os.path.isdir(steamdir) or not os.path.exists(steamdir):
+        sclogger.warning('Invalid or missing directory at %s', steamdir)
         steamdir = os.path.abspath(
             input('Invalid or missing directory, ' +
                   'please re-enter the directory: '))
 
     # Validate Steam installation path.
-    if os.path.isdir(steamdir) and 'Steam' in steamdir:
+    if os.path.isdir(steamdir):
+        if autolib:
+            for lib in get_libraries(steamdir):
+                liblist.append(lib)
         steamdir = fix_game_path(steamdir)
-        logger.info('Game installations located at %s', steamdir)
+        sclogger.info('Game installations located at %s', steamdir)
 
     # Gather game directories from default path.
-    logger.info('Checking %s', steamdir)
+    sclogger.info('Checking %s', steamdir)
     print('Checking %s' % (steamdir))
 
     # ensure the path provided exists and is a valid directory
@@ -152,27 +188,32 @@ def find_redist(steamdir, nodir=False, library=None):
                         gamedirs[os.path.join(steamdir, dir)] = ''
     # print directory to log if it is not found or invalid
     except FileNotFoundError:
-        logger.error('Directory %s is missing or invalid, skipping',
-                     steamdir)
+        sclogger.error('Directory %s is missing or invalid, skipping',
+                       steamdir)
         print('Directory %s is missing or invalid, skipping' % (steamdir))
 
     if library is not None:
         # split list is provided via cli application as a string
         if type(library) is str:
-            liblist = library.lower().split(',')
+            for lib in library.lower().split(','):
+                liblist.append(lib)
         else:
-            liblist = library
+            for lib in library:
+                liblist.append(lib)
+    else:
+        sclogger.info('No additional libraries provided.')
 
+    if len(liblist) > 0:
         # Check all provided libraries.
         for lib in liblist:
             # correct path issues and validate path
             lib = fix_game_path(lib)
             # Verify library path exists and is a directory
             if not os.path.exists(lib) or not os.path.isdir(lib):
-                logger.warning('Ignoring invalid directory at %s', lib)
+                sclogger.warning('Ignoring invalid directory at %s', lib)
                 continue
 
-            logger.info('Checking library at %s', lib)
+            sclogger.info('Checking library at %s', lib)
             print('Checking library at %s' % (lib))
 
             # build list of all valid game directories in each library
@@ -183,8 +224,6 @@ def find_redist(steamdir, nodir=False, library=None):
                         if libsubdir not in gamedirs:
                             # add key for each located directory
                             gamedirs[libsubdir] = ''
-    else:
-        logging.info('No additional libraries provided.')
 
     # nodir option to skip cleaning redistributable subdirectories
     if not nodir:
@@ -252,15 +291,15 @@ def find_redist(steamdir, nodir=False, library=None):
                                     cleanable[fpath] = (
                                         (os.path.getsize(fpath) / 1024) / 1024)
             except UnicodeDecodeError:
-                logging.error('Invalid characters found in file %s',
-                              vdffile)
+                sclogger.error('Invalid characters found in file %s',
+                               vdffile)
             except IndexError:
-                logging.error('Invalid data in file %s', vdffile)
+                sclogger.error('Invalid data in file %s', vdffile)
 
     # log all detected files and their size
     for file in cleanable:
-        logger.info('File %s found with size %s MB',
-                    file, format(cleanable[file], '.2f'))
+        sclogger.info('File %s found with size %s MB',
+                      file, format(cleanable[file], '.2f'))
 
     # Return the list of cleanable files and their approximate size.
     return cleanable
@@ -323,25 +362,25 @@ def clean_data(filelist, confirm=''):
                     if excludes and excludes.search(file):
                         # skip removal for excluded files
                         excluded += 1
-                        logger.info('%s excluded, skipping...', file)
+                        sclogger.info('%s excluded, skipping...', file)
                     else:
                         # only remove files which are not excluded
                         os.remove(file)
                         removed += 1
-                        logger.info('File %s removed successfully', file)
+                        sclogger.info('File %s removed successfully', file)
 
             except FileNotFoundError:
-                logger.error('File %s not found, skipping...', file)
+                sclogger.error('File %s not found, skipping...', file)
                 print('File %s not found, skipping.' % (file))
 
             except PermissionError:
-                logger.error('Permission denied to file %s skipping...',
-                             file)
+                sclogger.error('Permission denied to file %s skipping...',
+                               file)
                 print('Permission denied to file %s skipping...' % (file))
 
-        logger.info('%s file(s) removed successfully', removed)
-        logger.info('%s file(s) excluded and not removed', excluded)
-        logger.info('%s MB saved', format(totalsize, '.2f'))
+        sclogger.info('%s file(s) removed successfully', removed)
+        sclogger.info('%s file(s) excluded and not removed', excluded)
+        sclogger.info('%s MB saved', format(totalsize, '.2f'))
         print('\n%s file(s) removed successfully' % (removed))
         print('%s file(s) excluded and not removed' % (excluded))
         print('%s MB saved' % (format(totalsize, '.2f')))
@@ -357,9 +396,9 @@ def print_stats(cleanable):
     for cfile in cleanable:
         totalsize += float(cleanable[cfile])
 
-    logger.info('Total number of files marked for removal: %s', filecount)
-    logger.info('Estimated disk space saved after removal: %s MB',
-                format(totalsize, '.2f'))
+    sclogger.info('Total number of files marked for removal: %s', filecount)
+    sclogger.info('Estimated disk space saved after removal: %s MB',
+                  format(totalsize, '.2f'))
 
     print('\nTotal number of files marked for removal: %s' % filecount)
     print('Estimated disk space saved after removal: %s MB' %
@@ -378,6 +417,9 @@ if __name__ == "__main__":
     parser.add_argument('--nodir',
                         help='Do not clean redistributable directories.',
                         action='store_true')
+    parser.add_argument('--autolib',
+                        help='Attempt to auto detect Steam libraries in use.',
+                        action='store_true')
     parser.add_argument('-l', '--library',
                         help='Additional Steam libraries to examine '
                         '(comma separated).')
@@ -386,7 +428,8 @@ if __name__ == "__main__":
     print_header()
 
     if os.name == 'nt':
-        cleanable = find_redist(win_reg_check(), args.nodir, args.library)
+        cleanable = find_redist(win_reg_check(), args.autolib, args.nodir,
+                                args.library)
 
         if len(cleanable) > 0:
             if args.dryrun:
@@ -395,9 +438,7 @@ if __name__ == "__main__":
                 clean_data(cleanable)
         else:
             print('\nCongratulations! No files were found for removal. ')
-    elif os.name == 'posix':
-        print('No Linux support at this time.')
     else:
-        print('Invalid operating system detected.')
+        print('Invalid operating system detected, or not currently supported')
 
     input('\nPress Enter to exit...')
