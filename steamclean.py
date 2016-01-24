df8c60da548df12943e8f2e7c677c209ef91d839
evitalis <evitalis@users.noreply.github.com>
2016-01-24 08:17:55 -0600

    begin logic move to submodules

--- steamclean.py
+++ steamclean.py
@@ -5,6 +5,8 @@
 # Description:      Script to find and remove extraneous files from
 #                   Steam game installation directories.
 
+from providers import libsteam
+
 from codecs import StreamReader
 from datetime import datetime
 from platform import architecture as pa
@@ -24,7 +26,7 @@ sclogger = logging.getLogger('steamclean')
 sclogger.setLevel(logging.INFO)
 logformatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s',
                                  datefmt='%Y-%m-%d %H:%M:%S')
-# use current date and time for log file name for clarity
+# # use current date and time for log file name for clarity
 timenow = datetime.now().strftime('%Y%m%d-%H%M')
 fh = logging.FileHandler('steamclean_' + timenow + '.log')
 fh.setFormatter(logformatter)
@@ -56,53 +58,6 @@ def print_header(filename=None):
 
     print('Starting %s' % (header))
     print('Current operating system: %s %s\n' % (pp(), pa()[0]))
-
-
-def win_reg_check():
-    """ Get Steam installation path from reading registry data.
-        If unable to read registry information prompt user for input. """
-
-    arch = pa()[0]
-    regbase = 'HKEY_LOCAL_MACHINE\\'
-    regkey = None
-
-    # use architecture returned to evaluate appropriate registry key
-    if arch == '64bit':
-        regpath = r'SOFTWARE\Wow6432Node\Valve\Steam'
-        regopts = (winreg.KEY_WOW64_64KEY + winreg.KEY_READ)
-    elif arch == '32bit':
-        sclogger.info('32 bit operating system detected')
-
-        regpath = r'SOFTWARE\Valve\Steam'
-        regopts = winreg.KEY_READ
-    else:
-        sclogger.error('Unable to determine system architecture.')
-        raise ValueError('ERROR: Unable to determine system architecture.')
-
-    try:
-        regkey = winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE, regpath, 0,
-                                  regopts)
-        # Save installation path value and close open registry key.
-        ipath = winreg.QueryValueEx(regkey, 'InstallPath')[0]
-
-    except PermissionError:
-        sclogger.error('Unable to read registry data at %s due to insufficient \
-                 privileges.', regbase + regpath)
-        sclogger.error('Run this script as administrator to resolve.')
-        print('Permission denied to read registry data at %s.', regpath)
-
-        ipath = input('Please enter the Steam installation directory: ')
-
-    finally:
-        # Ensure registry key is closed after reading as applicable.
-        if regkey is not None:
-            sclogger.info('Registry data at %s used to determine ' +
-                          'installation path', regbase + regpath)
-            sclogger.info('Steam installation path found at %s', ipath)
-
-            winreg.CloseKey(regkey)
-
-        return ipath.strip()
 
 
 def fix_game_path(dir):
@@ -408,6 +363,8 @@ def print_stats(cleanable):
 
 
 if __name__ == "__main__":
+    prsteam = libsteam.ProviderSteam()
+
     parser = argparse.ArgumentParser(
         description='Find and clean extraneous files from game directories '
                     'including various Windows redistributables.')
@@ -428,8 +385,8 @@ if __name__ == "__main__":
     print_header()
 
     if os.name == 'nt':
-        cleanable = find_redist(win_reg_check(), args.autolib, args.nodir,
-                                args.library)
+        cleanable = find_redist(prsteam.winreg_read(), args.autolib,
+                                args.nodir, args.library)
 
         if len(cleanable) > 0:
             if args.dryrun:
